39b96b1e5f00880e16fb57e08bacc142decdbfbb
Sérgio da Rocha <sergiorajr1994@gmail.com>
2025-06-13 03:21:41 -0300

    remove unused classes

--- src/main/java/com/abigurumi/abigurumiapi/repository/AbigurumiRepository.java
+++
@@ -1,7 +0,0 @@
-/*package com.abigurumi.abigurumiapi.repository;
-
-   import com.abigurumi.abigurumiapi.entity.AbigurumiEntity;
-   import org.springframework.data.jpa.repository.JpaRepository;
-
-   public interface AbigurumiRepository extends JpaRepository<AbigurumiEntity, Long> {
-   }*/
